09f20593e2ef695844399e4eafb78dc24455b98a
Marcus Johnson <bumblebritches57@gmail.com>
2017-03-23 16:37:51 -0400

    Added InitPCMFile and ClosePCMFile function

--- libPCM/include/libPCM.h
+++ libPCM/include/libPCM.h
@@ -18,26 +18,34 @@ extern "C" {
         W64HeaderSize       = 104,
         WAVFormatExtensible = 0xFFFE,
     };
-    
+
     enum SpeakerMask {
-        FrontRight       =     0x1,
-        FrontLeft        =     0x2,
-        FrontCenter      =     0x4,
-        LFE              =     0x8,
-        BackLeft         =    0x10,
-        BackRight        =    0x20, // yes
-        FrontLeftCenter  =    0x40, // yes
-        FrontRightCenter =    0x80,
-        BackCenter       =   0x100,
-        SideLeft         =   0x200,
-        SideRight        =   0x400,
-        TopCenter        =   0x800,
-        TopFrontLeft     =  0x1000,
-        TopFrontCenter   =  0x2000,
-        TopFrontRight    =  0x4000, // yes
-        TopBackLeft      =  0x8000,
-        TopBackCenter    = 0x10000,
-        TopBackRight     = 0x20000,
+        FrontRight          = 0x1,
+        FrontLeft           = 0x2,
+        FrontCenter         = 0x4,
+        LFE                 = 0x8,
+        BackLeft            = 0x10,
+        BackRight           = 0x20,// yes
+        FrontLeftCenter     = 0x40,// yes
+        FrontRightCenter    = 0x80,
+        BackCenter          = 0x100,
+        SideLeft            = 0x200,
+        SideRight           = 0x400,
+        TopCenter           = 0x800,
+        TopFrontLeft        = 0x1000,
+        TopFrontCenter      = 0x2000,
+        TopFrontRight       = 0x4000,// yes
+        TopBackLeft         = 0x8000,
+        TopBackCenter       = 0x10000,
+        TopBackRight        = 0x20000,
+    };
+
+    enum PCMFileTypes {
+        Unknown_Type        = 0,
+        WAV_File            = 1,
+        RF64_File           = 2,
+        W64_File            = 3,
+        AIFF_File           = 4,
     };
     
     typedef struct W64Header W64Header;
@@ -50,21 +58,15 @@ extern "C" {
     
     typedef struct PCMMetadata PCMMetadata;
     
-    enum PCMFileTypes {
-        Unknown_Type = 0,
-        WAV_File     = 1,
-        RF64_File    = 2,
-        W64_File     = 3,
-        AIFF_File    = 4,
-    };
-    
     // So if it's a WAV file the input file metadata reader thing will set it to WAV, then read the meta into Meta
     // Then once it comes across the equilivent of a data chunk, it will set Data to the pointer, then for extracting
     // samples it will read the type call the correct reader and extract the requested samples.
     
-    void IdentifyPCMFile(BitInput *BitI, PCMFile *PCM);
-    void ParseWAVFile(BitInput *BitI, PCMFile *PCM);
-    void ParseW64File(BitInput *BitI, W64Header *W64);
+    void     IdentifyPCMFile(BitInput *BitI, PCMFile *PCM);
+    void     ParseWAVFile(BitInput *BitI, PCMFile *PCM);
+    void     ParseW64File(BitInput *BitI, W64Header *W64);
+    PCMFile *InitPCMFile(void);
+    void     ClosePCMFile(PCMFile *PCM);
     
 #ifdef __cplusplus
 }
