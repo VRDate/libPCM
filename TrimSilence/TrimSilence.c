4dd254b7beec09e16d045824a5b0c86d2ea66722
Marcus Johnson <bumblebritches57@gmail.com>
2017-05-15 05:49:27 -0400

    Updated function calls in TrimSilence

--- TrimSilence/TrimSilence.c
+++ TrimSilence/TrimSilence.c
@@ -18,21 +18,21 @@ extern "C" {
         SetCMDLicenseURL(CMD, "https://opensource.org/licenses/BSD-3-Clause", false);
         SetCMDMinSwitches(CMD, 3);
         
-        SetSwitchFlag(CMD, 0, "Input", 5);
-        SetSwitchDescription(CMD, 0, "Input file or stdin with: '-'\n");
-        SetSwitchResultStatus(CMD, 0, false);
+        SetCMDSwitchFlag(CMD, 0, "Input", 5);
+        SetCMDSwitchDescription(CMD, 0, "Input file or stdin with: '-'\n");
+        SetCMDSwitchResultStatus(CMD, 0, false);
         
-        SetSwitchFlag(CMD, 1, "Output", 6);
-        SetSwitchDescription(CMD, 1, "Output file or stdout with: '-'\n");
-        SetSwitchResultStatus(CMD, 1, false);
+        SetCMDSwitchFlag(CMD, 1, "Output", 6);
+        SetCMDSwitchDescription(CMD, 1, "Output file or stdout with: '-'\n");
+        SetCMDSwitchResultStatus(CMD, 1, false);
         
-        SetSwitchFlag(CMD, 2, "Silence", 7);
-        SetSwitchDescription(CMD, 2, "Set the threshhold, in dB or absolute value like: (-|--|/)Silence 12dB or: (-|--|/)Silence 0");
-        SetSwitchResultStatus(CMD, 2, false);
+        SetCMDSwitchFlag(CMD, 2, "Silence", 7);
+        SetCMDSwitchDescription(CMD, 2, "Set the threshhold, in dB or absolute value like: (-|--|/)Silence 12dB or: (-|--|/)Silence 0");
+        SetCMDSwitchResultStatus(CMD, 2, false);
         
-        SetSwitchFlag(CMD, 3, "Help", 4);
-        SetSwitchDescription(CMD, 6, "Prints all the command line options");
-        SetSwitchResultStatus(CMD, 6, true);
+        SetCMDSwitchFlag(CMD, 3, "Help", 4);
+        SetCMDSwitchDescription(CMD, 6, "Prints all the command line options");
+        SetCMDSwitchResultStatus(CMD, 6, true);
         
         return CMD;
     }
