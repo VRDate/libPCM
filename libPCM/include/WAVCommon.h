a443f67a705747d080269c0f04f2c4a33b5d94b0
Marcus Johnson <bumblebritches57@gmail.com>
2017-07-08 06:16:07 -0400

    Reorganized function parameter order.

--- libPCM/include/WAVCommon.h
+++ libPCM/include/WAVCommon.h
@@ -9,16 +9,16 @@
 extern "C" {
 #endif
     
-    void ReadINFO_IART(BitBuffer *BitB, PCMFile *PCM, uint32_t ChunkSize);
-    void ReadINFO_ICRD(BitBuffer *BitB, PCMFile *PCM, uint32_t ChunkSize);
-    void ReadINFO_IGNR(BitBuffer *BitB, PCMFile *PCM, uint32_t ChunkSize);
-    void ReadINFO_INAM(BitBuffer *BitB, PCMFile *PCM, uint32_t ChunkSize);
-    void ReadINFO_IPRD(BitBuffer *BitB, PCMFile *PCM, uint32_t ChunkSize);
-    void ReadINFO_ISFT(BitBuffer *BitB, PCMFile *PCM, uint32_t ChunkSize);
-    void ParseWavLISTChunk(BitBuffer *BitB, PCMFile *PCM, uint32_t ChunkSize);
-    void ParseWavDATAChunk(BitBuffer *BitB, PCMFile *PCM, uint32_t ChunkSize);
-    void ParseWavFMTChunk(BitBuffer *BitB, PCMFile *PCM, uint32_t ChunkSize);
-    void WAVExtractSamples(BitBuffer *BitB, PCMFile *PCM, uint64_t NumSamples2Extract);
+    void ReadINFO_IART(WAVHeader *Wav, BitBuffer *BitB, uint32_t ChunkSize);
+    void ReadINFO_ICRD(WAVHeader *Wav, BitBuffer *BitB, uint32_t ChunkSize);
+    void ReadINFO_IGNR(WAVHeader *Wav, BitBuffer *BitB, uint32_t ChunkSize);
+    void ReadINFO_INAM(WAVHeader *Wav, BitBuffer *BitB, uint32_t ChunkSize);
+    void ReadINFO_IPRD(WAVHeader *Wav, BitBuffer *BitB, uint32_t ChunkSize);
+    void ReadINFO_ISFT(WAVHeader *Wav, BitBuffer *BitB, uint32_t ChunkSize);
+    void ParseWavLISTChunk(WAVHeader *Wav, BitBuffer *BitB, uint32_t ChunkSize);
+    void ParseWavDATAChunk(WAVHeader *Wav, BitBuffer *BitB, uint32_t ChunkSize);
+    void ParseWavFMTChunk(WAVHeader *Wav, BitBuffer *BitB, uint32_t ChunkSize);
+    void WAVExtractSamples(WAVHeader *Wav, BitBuffer *BitB, uint64_t NumSamples2Extract);
     
 #ifdef __cplusplus
 }
